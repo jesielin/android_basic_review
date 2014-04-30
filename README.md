06281251720f503fd142c281a848647e734e425f
Shawn <Shawn@Shawn-PC>
2014-05-01 00:13:07 +0800

    电话拨号器

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ android_basic_review
 ====================
 
 some android basic demos review
+
+一些android基础的小Demo，顺便熟悉git操作
+有些工程会用到xutils开源框架
---
+++ 电话拨号器/src/com/example/tel/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.tel;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+		
+		@ViewInject(R.id.et)
+		private EditText et;
+		
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			
+			ViewUtils.inject(this, rootView);
+			
+			return rootView;
+		}
+		@OnClick(R.id.bn)
+		public void call(View view){
+			String number = et.getText().toString();
+			if(TextUtils.isEmpty(number)){
+				Toast.makeText(getActivity(), "ºÅÂë²»ÄÜÎª¿Õ", 0).show();
+				return;
+			}
+			 Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:"+ number));
+		     startActivity(intent);
+
+		}
+	}
+	
+
+}
