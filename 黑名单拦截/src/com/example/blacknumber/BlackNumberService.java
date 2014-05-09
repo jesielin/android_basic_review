7b4f2fa1eb79c2624a51cbe1e1fc32e17f704096
Shawn <fengxiaojun1990@qq.com>
2014-05-10 02:22:36 +0800

    Git演示

---
+++ 黑名单拦截/src/com/example/blacknumber/BlackNumberService.java
@@ -0,0 +1,24 @@
+package com.example.blacknumber;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BlackNumberService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+}
